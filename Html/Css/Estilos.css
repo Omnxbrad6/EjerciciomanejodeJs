f55f93fcf271f9a6e7aba80b2618b56771706ea0
Omnxbrad6 <omnxuri06@outlook.com>
2024-09-22 17:48:10 -0600

    inserte codigo css

---
+++ Html/Css/Estilos.css
@@ -0,0 +1,23 @@
+h1{
+    color: red;
+    text-align: center;
+}
+
+
+#banner{
+    display: flex;
+    height: 100%;
+    background-color: aqua;
+}
+
+table{
+    border-collapse: separate;
+    border-width: 3px;
+    border-style: solid;
+}
+
+td{
+    border-style: solid;
+    border-color: blue;
+    border-width: 1px;
+}
